ff73755e88f6489b883b1f0592a80a86b099460e
Maksim <maksimkuzin73@yandex.ru>
2022-12-21 10:17:53 +0400

    Add SacredEvent logic

--- CourseWorkDB/Controllers/DataBaseController.cs
+++ CourseWorkDB/Controllers/DataBaseController.cs
@@ -208,6 +208,8 @@ namespace CourseWorkDB.Controllers
             {
                 case "Добавить службу":
                     return CreateDivineService();
+                case "Добавить священное событие":
+                    return CreateSacredEvent();
                 case "Редактировать мероприятие":
                     return EditEvent(ids);
                 case "Удалить мероприятие":
@@ -255,26 +257,86 @@ namespace CourseWorkDB.Controllers
             return View("../DivineService/Index", model);
         }
         [HttpGet]
+        public ActionResult CreateSacredEvent()
+        {
+            return View("../SacredEvent/Create");
+        }
+        [HttpPost]
+        public ActionResult CreateSacredEvent(string name, DateTime date, int priestId, string place, DateTime finishDate, string transport, string route,string sourceOfFunding)
+        {
+            var @event = new Event
+            {
+                Name = name,
+                Date = date,
+                PriestId = priestId,
+                Type = "Священное событие"
+            };
+            db.Events.Add(@event);
+            db.SaveChanges();
+            int maxId = db.Events.Max(e => e.Id);
+            @event = db.Events.SingleOrDefault(e => e.Id == maxId);
+            var sEvent = new SacredEvent
+            {
+                Place = place,
+                FinishDate = finishDate,
+                Route = route,
+                SourceOfFunding = sourceOfFunding,
+                Transport = transport,
+                EventId = maxId,
+                Event = @event
+            };
+            db.SacredEvents.Add(sEvent);
+            db.SaveChanges();
+            var model = db.Events;
+            return View("../Event/Index", model);
+        }
+        public ActionResult AboutSacredEvent(int id)
+        {
+            var model = db.Events.SingleOrDefault(e => e.Id == id);
+            var sEvent = db.SacredEvents.SingleOrDefault(e => e.EventId == id);
+            model.SacredEvent = sEvent;
+            return View("../SacredEvent/Index", model);
+        }
+        [HttpGet]
         public ActionResult EditEvent(int id)
         {
             var model = db.Events.SingleOrDefault(e=>e.Id==id);
             var divineService = db.DivineServices.SingleOrDefault(e => e.EventId == id);
             if (divineService != null)
                 model.DivineService = divineService;
+            var sEvent = db.SacredEvents.SingleOrDefault(e => e.EventId == id);
+            if (sEvent != null)
+                model.SacredEvent = sEvent;
             return View("../Event/Edit",model);
         }
-        public ActionResult EditEvent(int id, string name, DateTime date, int priestId, string justification, string prayer)
+        [HttpPost]
+        public ActionResult EditEvent(int id, string name, DateTime date, int priestId, string justification, string prayer, string place, DateTime finishDate, string transport, string route, string sourceOfFunding)
         {
             var @event = db.Events.SingleOrDefault(e => e.Id == id);
             @event.Name = name;
             @event.Date = date;
             @event.PriestId = priestId;
             var divineService = db.DivineServices.SingleOrDefault(e => e.EventId == id);
-            divineService.Justification = justification;
-            divineService.Prayer = prayer;
-            db.Events.Update(@event);
-            db.DivineServices.Update(divineService);
-            db.SaveChanges();
+            if (divineService != null)
+            {
+                divineService.Justification = justification;
+                divineService.Prayer = prayer;
+                db.Events.Update(@event);
+                db.DivineServices.Update(divineService);
+                db.SaveChanges();
+            }
+            var sEvent = db.SacredEvents.SingleOrDefault(e => e.EventId == id);
+            if(sEvent != null)
+            {
+                sEvent.Place = place;
+                sEvent.FinishDate = finishDate;
+                sEvent.Route = route;
+                sEvent.SourceOfFunding = sourceOfFunding;
+                sEvent.Transport = transport;
+                db.Events.Update(@event);
+                db.SacredEvents.Update(sEvent);
+                db.SaveChanges();
+            }
             var model = db.Events;
             return View("../Event/Index", model);
         }
